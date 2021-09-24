d777ab8e3cca3f809250b8727b6c4ba9aa223c07
Micaela Lisi <micaela.lisi@ko2.it>
2021-09-24 15:21:46 +0200

    initial spotify

--- main.tf
+++ main.tf
@@ -1,38 +1,36 @@
 terraform {
   required_providers {
     spotify = {
-      version = "~> 0.1.5"
+      version = "~> 0.2.6"
       source  = "conradludgate/spotify"
     }
   }
 }
 
-variable "spotify_api_key" {
-  type = string
-}
-
 provider "spotify" {
+  auth_server = "https://oauth2.conrad.cafe"
   api_key = var.spotify_api_key
+  username = "micaela.lisi"
+  token_id = "dc21acd5-ed7b-45cd-943c-83253dc5bb11"
 }
 
 resource "spotify_playlist" "playlist" {
-  name        = "Terraform Summer Playlist"
+  name        = "Terraform Micaela Playlist"
   description = "This playlist was created by Terraform"
   public      = true
 
-  tracks = [
-    data.spotify_search_track.by_artist.tracks[0].id,
-    data.spotify_search_track.by_artist.tracks[1].id,
-    data.spotify_search_track.by_artist.tracks[2].id,
-  ]
+  tracks = flatten([
+    data.spotify_search_track.search.tracks[0].id,
+  ])
 }
 
-data "spotify_search_track" "by_artist" {
-  artists = ["Dolly Parton"]
-  #  album = "Jolene"
-  #  name  = "Early Morning Breeze"
+
+data "spotify_search_track" "search" {
+  name   = "Somebody Told Me"
+  artist = "The Killers"
+  album  = "Hot Fuss"
 }
 
-output "tracks" {
-  value = data.spotify_search_track.by_artist.tracks
+output "test" {
+  value = data.spotify_search_track.search.tracks
 }
---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+spotify_api_key = "8Yg9W4piSf_rnNqmJVLEtf5v7Ymk_HrjETmfoVjz2jGf6NRkcsuV2VGj5rzdXqBh"
